3bb23418664e39239f7bd4f80be833651083f5eb
albert <albertejuku92@gmail.com>
2022-11-20 22:56:16 +0300

    Initial commit

---
+++ src/main/java/com/eikal/EikalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eikal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EikalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EikalBackendApplication.class, args);
+    }
+
+}
