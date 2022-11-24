5640e2dd106468612e5a5e8b054a90ddc69fe48d
albert <albertejuku92@gmail.com>
2022-11-24 11:34:08 +0300

    Initial Commit

--- src/main/java/com/eikal/service/places/ContactInformationService.java
+++ src/main/java/com/eikal/service/places/ContactInformationService.java
@@ -1,10 +1,19 @@
 package com.eikal.service.places;
 
+import com.eikal.models.places.ContactInformation;
+import com.eikal.repository.places.ContactInformationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContactInformationService {
 
+    private final ContactInformationRepository contactInformationRepository;
 
+    public ContactInformationService(ContactInformationRepository contactInformationRepository) {
+        this.contactInformationRepository = contactInformationRepository;
+    }
 
+    public ContactInformation save(ContactInformation contactInformation) {
+        return contactInformationRepository.save(contactInformation);
+    }
 }
