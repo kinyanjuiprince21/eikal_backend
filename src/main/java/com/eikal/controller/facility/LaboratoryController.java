1b746ec98522e607428302d9020f0605fe0c455e
albert <albertejuku92@gmail.com>
2022-12-08 22:34:26 +0300

    Initial Commit

--- src/main/java/com/eikal/controller/facility/LaboratoryController.java
+++ src/main/java/com/eikal/controller/facility/LaboratoryController.java
@@ -1,7 +1,11 @@
 package com.eikal.controller.facility;
 
+import com.eikal.models.facility.Laboratory;
 import com.eikal.service.facility.LaboratoryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class LaboratoryController {
@@ -12,5 +16,10 @@ public class LaboratoryController {
         this.labService = labService;
     }
 
-
+    public ResponseEntity<?> save(Map<String, Object> map) {
+        Laboratory laboratory = labService.saveLab(map);
+        return laboratory != null ?
+                ResponseEntity.status(201).body(laboratory) :
+                ResponseEntity.status(415).build();
+    }
 }
