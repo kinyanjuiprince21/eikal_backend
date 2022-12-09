be25a216eeed0a73154b0e2c096538157459aed8
albert <albertejuku92@gmail.com>
2022-12-09 15:23:47 +0300

    Initial Commit

--- src/main/java/com/eikal/controller/facility/LaboratoryController.java
+++ src/main/java/com/eikal/controller/facility/LaboratoryController.java
@@ -5,6 +5,7 @@ import com.eikal.service.facility.LaboratoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,6 +23,28 @@ public class LaboratoryController {
                 ResponseEntity.status(201).body(laboratory) :
                 ResponseEntity.status(415).build();
     }
+
+    public ResponseEntity<?> findLab(Long id) {
+        Laboratory laboratory = labService.findLab(id);
+        return laboratory != null ?
+                ResponseEntity.status(200).body(laboratory) :
+                ResponseEntity.status(404).body("Laboratory not found");
+    }
+
+    public ResponseEntity<?> findLabsInFacility(Long id) {
+        List<Laboratory> laboratories = labService.findLabsInFacility(id);
+        return !laboratories.isEmpty() ?
+                ResponseEntity.status(200).body(laboratories) :
+                ResponseEntity.status(404).body("No laboratories in this facility");
+    }
+
+    public ResponseEntity<?> findLabsInDepartment(Long id) {
+        List<Laboratory> laboratories = labService.findLabsInDepartment(id);
+        return !laboratories.isEmpty() ?
+                ResponseEntity.status(200).body(laboratories) :
+                ResponseEntity.status(404).body("No laboratories in this department");
+    }
+
 
 
 }
