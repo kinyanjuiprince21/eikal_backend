00f14110bdde5985666e920d7d07e4cca72fd071
albert <albertejuku92@gmail.com>
2022-12-09 15:28:42 +0300

    Initial Commit

--- src/main/java/com/eikal/controller/facility/LaboratoryController.java
+++ src/main/java/com/eikal/controller/facility/LaboratoryController.java
@@ -3,6 +3,8 @@ package com.eikal.controller.facility;
 import com.eikal.models.facility.Laboratory;
 import com.eikal.service.facility.LaboratoryService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,20 +26,23 @@ public class LaboratoryController {
                 ResponseEntity.status(415).build();
     }
 
-    public ResponseEntity<?> findLab(Long id) {
+    @GetMapping("laboratory")
+    public ResponseEntity<?> findLab(@RequestParam("id") Long id) {
         Laboratory laboratory = labService.findLab(id);
         return laboratory != null ?
                 ResponseEntity.status(200).body(laboratory) :
                 ResponseEntity.status(404).body("Laboratory not found");
     }
 
-    public ResponseEntity<?> findLabsInFacility(Long id) {
+    @GetMapping("laboratory/facility")
+    public ResponseEntity<?> findLabsInFacility(@RequestParam("id") Long id) {
         List<Laboratory> laboratories = labService.findLabsInFacility(id);
         return !laboratories.isEmpty() ?
                 ResponseEntity.status(200).body(laboratories) :
                 ResponseEntity.status(404).body("No laboratories in this facility");
     }
 
+    @GetMapping("laboratory/department")
     public ResponseEntity<?> findLabsInDepartment(Long id) {
         List<Laboratory> laboratories = labService.findLabsInDepartment(id);
         return !laboratories.isEmpty() ?
